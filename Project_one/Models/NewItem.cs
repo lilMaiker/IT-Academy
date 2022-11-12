b1b0fc9222675496dda8a4d02936f389e8c4fc77
lilMaiker <fuhega45@gmail.com>
2022-11-12 17:40:36 +0300

    Add container class NewItem.

---
+++ Project_one/Models/NewItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_one.Models
+{
+    internal class NewItem
+    {
+        public string Name { get; set; }    
+    }
+}
