074d812836dc769ce936ff163eb33ab8dc383460
lilMaiker <fuhega45@gmail.com>
2022-11-12 17:39:09 +0300

    Добавьте файлы проекта.

---
+++ Project_one/Program.cs
@@ -0,0 +1,29 @@
+﻿using Project_one.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Project_one
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+            Console.WriteLine(jsonString);
+            Console.ReadLine();
+        }
+    }
+}
